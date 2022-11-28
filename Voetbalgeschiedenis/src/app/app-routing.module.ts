82569468ec414fa47f26f135a87919d39346aca4
RobbertBons <74967915+RobbertBons@users.noreply.github.com>
2022-11-28 11:20:20 +0100

    routerlink

--- Voetbalgeschiedenis/src/app/app-routing.module.ts
+++ Voetbalgeschiedenis/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomepageComponent
+  },
+  {
+    path: 'users',
+    component: 
+  },
+  {
+    path: 'details',
+    component: 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
