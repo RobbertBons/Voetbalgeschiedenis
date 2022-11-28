0d87acac67d37d6740b1a90962b05c320b9ee1ea
RobbertBons <74967915+RobbertBons@users.noreply.github.com>
2022-11-28 11:10:03 +0100

    CSS toegevoegd

--- Voetbalgeschiedenis/src/app/app.module.ts
+++ Voetbalgeschiedenis/src/app/app.module.ts
@@ -3,13 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+<<<<<<< HEAD
 import { NavComponent } from './nav/nav.component';
 import { HttpClientModule } from '@angular/common/http';
+=======
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HomepageComponent } from './homepage/homepage.component';
+>>>>>>> Develop
 
 @NgModule({
   declarations: [
     AppComponent,
+<<<<<<< HEAD
     NavComponent
+=======
+    SidebarComponent,
+    HomepageComponent
+>>>>>>> Develop
   ],
   imports: [
     BrowserModule,
--- Voetbalgeschiedenis/src/app/json.service.ts
+++ Voetbalgeschiedenis/src/app/json.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+<<<<<<< HEAD
+=======
+import { HttpClientModule } from '@angular/common/http';
+>>>>>>> Develop
 
 @Injectable({
   providedIn: 'root'
 })
 export class JsonService {
 
+<<<<<<< HEAD
   constructor() { }
+=======
+  constructor(private http: HttpClientModule) { }
+
+  
+>>>>>>> Develop
 }
